7f9b208b8634d468cfef263b7092f8ccc29089f6
Trifa <trifa@hku.hk>
2021-10-29 12:20:02 +0800

    Gradle Update

--- starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -1,32 +1,48 @@
 package com.udacity.asteroidradar.detail
 
-
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentDetailBinding
 
+/**
+ * The class contain the fragment UI of the Asteroid detail.
+ * */
 class DetailFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentDetailBinding.inflate(inflater)
-        binding.lifecycleOwner = this
-
-        val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
-
-        binding.asteroid = asteroid
-
-        binding.helpButton.setOnClickListener {
-            displayAstronomicalUnitExplanationDialog()
-        }
-
+        initBinding()
+        initClickListener()
         return binding.root
     }
 
+
+//************************************* Initialization *********************************************
+
+    private fun initBinding() {
+        // Connect the class with XML UI.
+        binding.lifecycleOwner = this
+
+        // Get the Data from the list fragment.
+        val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
+
+        // asteroid is the data binding in the detail fragment.
+        binding.asteroid = asteroid
+    }
+
+    private fun initClickListener() {
+        binding.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+    }
+
+
+    // The function is usd to pop up a dialog.
     private fun displayAstronomicalUnitExplanationDialog() {
         val builder = AlertDialog.Builder(activity!!)
             .setMessage(getString(R.string.astronomica_unit_explanation))

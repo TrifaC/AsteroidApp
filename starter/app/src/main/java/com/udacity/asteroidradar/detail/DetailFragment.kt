794a03ae910cc772db8a47191c30fe65ec209ffc
Trifa <trifa@hku.hk>
2021-10-29 12:23:30 +0800

    Stable start version final update.

--- starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.asteroidradar.detail
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 
@@ -12,6 +13,7 @@ import com.udacity.asteroidradar.databinding.FragmentDetailBinding
  * The class contain the fragment UI of the Asteroid detail.
  * */
 class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -44,7 +46,7 @@ class DetailFragment : Fragment() {
 
     // The function is usd to pop up a dialog.
     private fun displayAstronomicalUnitExplanationDialog() {
-        val builder = AlertDialog.Builder(activity!!)
+        val builder = AlertDialog.Builder(requireActivity())
             .setMessage(getString(R.string.astronomica_unit_explanation))
             .setPositiveButton(android.R.string.ok, null)
         builder.create().show()
